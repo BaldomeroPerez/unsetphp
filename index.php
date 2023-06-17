0f09b987472138ae462b6047be84349ba05a851b
BaldomeroPerez <70609133+BaldomeroPerez@users.noreply.github.com>
2023-06-16 22:27:12 -0300

    aprendendo comentarios

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+
+// forma de fazer anotações e observações
+
+// Comentario de uma linha.
+
+# Comentario de uma linha.
+
+/* Comentario de varias linha.
+    linha 1
+    linha 2
+    linha 3 
+*/
+
+?>
